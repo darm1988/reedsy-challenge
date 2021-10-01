0178dd658c7e1547687e359fae19c11097bc1cec
Dan R <daniel@Dans-MacBook-Pro.local>
2021-09-30 21:30:03 -0400

    item update endpoint

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :find_item, only: [:update]
+
   def index
     render json: Item.all
+  end
+
+  def update
+    @item.update!(item_params)
+
+    render json: @item
+  end
+
+  private
+
+  def find_item
+    item_id = params[:id]
+    @item = Item.find(item_id)
+  end
+
+  def item_params
+    params.permit(:price)
   end
 end
